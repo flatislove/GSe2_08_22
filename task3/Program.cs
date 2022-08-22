73b5525ba4ab2e35d842fd73bf22142e9efc6032
flvtisv <flatislove@gmail.com>
2022-08-23 00:14:57 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int number = 0;
+
+while (true)
+{
+    Console.WriteLine("Введите число, соответствующее дню недели");
+    if (int.TryParse(Console.ReadLine(), out number) && number < 8 && number > 0)
+    {
+        break;
+    }
+    else Console.WriteLine("Некорректный ввод");
+}
+Console.WriteLine(number < 6 ? "Выходной? Нет(" : "Выходной? Да)");
