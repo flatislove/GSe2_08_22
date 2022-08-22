40ec453bef882edf9d814d7b5b04499c06c6effd
flvtisv <flatislove@gmail.com>
2022-08-23 00:02:17 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿int result(int number)
+{
+    number = Math.Abs(number);
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+Console.Clear();
+int number = 0;
+
+while (true)
+{
+    Console.WriteLine("Введите число");
+    if (int.TryParse(Console.ReadLine(), out number))
+    {
+        break;
+    }
+    else Console.WriteLine("Некорректный ввод");
+}
+Console.WriteLine((number > 99 || number < -99) ? result(number) : "Нет третьего числа");
