d53f0b22e519c6385d1a8b437c37800bb1feb761
flvtisv <flatislove@gmail.com>
2022-08-22 22:49:46 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+int number = 0;
+while (true)
+{
+    Console.WriteLine("Введите трехзначное число");
+    try
+    {
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Некорректное значение. Введите трехзначное число");
+    }
+
+    if ((number > 99 && number < 1000) || (number < -99 && number > -1000))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Число не трехзначное");
+    }
+}
+int number2 = (number / 10) % 10;
+Console.WriteLine(Math.Abs(number2));
